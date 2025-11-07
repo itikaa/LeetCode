977f1e808df94350df98800c5f283d384b490d0b
Itika goel <147130718+itikaa@users.noreply.github.com>
2025-11-07 20:02:57 +0530

    Time: 2777 ms (5.01%), Space: 41.9 MB (5.85%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numTrees(int n) {
+        if(n==0 || n==1){return 1;}
+        int sum=0;      
+      for(int i=1;i<=n;i++){
+        int left=i-1;
+        int right=n-i;
+         sum+=( numTrees(left)*numTrees(right));
+       }
+       return sum;
+    }
+}
+
+
+
+
+
+
+
+
+  
