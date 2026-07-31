331746b74d7f764c7ccde1a2c5a6665c9dc61559
Itika goel <147130718+itikaa@users.noreply.github.com>
2026-07-31 16:05:49 +0530

    Time: 47 ms (16.99%), Space: 15.4 MB (10.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,22 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        #nums.sort()
-        l=0
-        r=0
+        freq={}
         n=len(nums)
         sum=0
         ans=0
-        while(r<n):
-            sum+=nums[r]
-            while(l<r and (sum>k or sum<0)):
-               sum-=nums[l]
-               l+=1
+        for i in range(0,n):
+            sum+=nums[i]
             if(sum==k):
                 ans+=1
-            r+=1
+            if((sum-k)in freq):
+                ans+=freq[sum-k]
+            freq[sum]=freq.get(sum,0)+1
         return ans
-            
-
-
-        
